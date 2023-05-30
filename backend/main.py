40db75ea753e385eb1830a72ffe198439335271d
rohin <rs.rohinsood@gmail.com>
2023-05-30 12:51:58 -0700

    suppress allow unsafe warning

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
 from monkey import app, socketio
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, port=8080)
+    socketio.run(app, debug=True, port=8080, allow_unsafe_werkzeug=True)
